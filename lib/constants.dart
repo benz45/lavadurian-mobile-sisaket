736d9179cb0088585f9749f3e5c34dd6ba976b7b
Veerapan Boonbuth <benz45.th@outlook.com>
2021-02-17 16:43:09 +0700

    On Submit and Validate ID Card Screen

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 // Colors Content.
 const kPrimaryColor = Color(0xFF005cae);
+const kDisabledPrimaryColor = Color(0xDFE3EE);
 const kPrimaryLightColor = Color(0xFFdfe1ee);
 
 // Colors Text.
