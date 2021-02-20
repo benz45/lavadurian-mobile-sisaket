6a3ce4a70936f371664c05b30fb444d691d3dea1
Veerapan Boonbuth <benz45.th@outlook.com>
2021-02-20 22:47:17 +0700

    Fix logout add clear store user model on provider

--- lib/models/profile_model.dart
+++ lib/models/profile_model.dart
@@ -5,6 +5,9 @@ class UserModel extends ChangeNotifier {
 
   Map<String, dynamic> get value => _value;
 
+  // Clear State
+  void clear() => {_value.clear(), notifyListeners()};
+
   set value(Map<String, dynamic> value) {
     _value = value;
     notifyListeners();
