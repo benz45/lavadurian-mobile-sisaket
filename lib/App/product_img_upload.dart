5a7633fdb94ac19e877a2134b215dbefe6c6d481
Phisan Sookkhee <phisan.shukkhi@gmail.com>
2021-02-25 10:32:23 +0700

    uploader screen completed

--- lib/App/product_img_upload.dart
+++ lib/App/product_img_upload.dart
@@ -1,3 +1,4 @@
+import 'package:LavaDurian/constants.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:multi_image_picker/multi_image_picker.dart';
@@ -69,42 +70,60 @@ class _ProductImageUploadState extends State<ProductImageUpload> {
 
   @override
   Widget build(BuildContext context) {
+    RaisedButton uploadButton = RaisedButton(
+      onPressed: () {},
+      color: kPrimaryColor,
+      shape: new RoundedRectangleBorder(
+        borderRadius: new BorderRadius.circular(10.0),
+      ),
+      child: Text(
+        'Upload Photo',
+        style: TextStyle(color: Colors.white, fontSize: 16),
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Flutter Image Picker Example"),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Center(
-            child: Text(
-              "ภาพผลิตภัณฑ์สำหรับประชาสัมพันธ์",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              height: 400.0,
-              child: Center(
-                child: images.isEmpty
-                    ? Text("ยังไม่มีภาพที่ถูกเลือก")
-                    : buildGridView(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 40.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: Text(
+                "ภาพผลิตภัณฑ์สำหรับประชาสัมพันธ์",
+                style: TextStyle(fontSize: 20),
               ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              FloatingActionButton(
-                onPressed: () => loadAssets(),
-                tooltip: "pickImage",
-                child: Icon(Icons.add_a_photo),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: 400.0,
+                child: Center(
+                  child: images.isEmpty
+                      ? Text("ยังไม่มีภาพที่ถูกเลือก")
+                      : buildGridView(),
+                ),
               ),
-            ],
-          )
-        ],
+            ),
+            images.isNotEmpty
+                ? ButtonBar(
+                    alignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      uploadButton,
+                    ],
+                  )
+                : Text("")
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => loadAssets(),
+        tooltip: "pickImage",
+        child: Icon(Icons.add_a_photo),
       ),
     );
   }
