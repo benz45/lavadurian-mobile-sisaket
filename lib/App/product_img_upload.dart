c84ccf2ba60573f9f31909b78d9a083ee7dd4664
Phisan Sookkhee <phisan.shukkhi@gmail.com>
2021-02-25 10:11:14 +0700

    update multi_image_picker version

--- lib/App/product_img_upload.dart
+++ lib/App/product_img_upload.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
-
 import 'package:multi_image_picker/multi_image_picker.dart';
 
 class ProductImageUpload extends StatefulWidget {
@@ -20,13 +19,20 @@ class _ProductImageUploadState extends State<ProductImageUpload> {
   Widget buildGridView() {
     if (images != null)
       return GridView.count(
-        crossAxisCount: 3,
+        crossAxisCount: 1,
         children: List.generate(images.length, (index) {
           Asset asset = images[index];
-          return AssetThumb(
-            asset: asset,
-            width: 300,
-            height: 300,
+          return Card(
+            semanticContainer: true,
+            clipBehavior: Clip.antiAliasWithSaveLayer,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
+            margin: EdgeInsets.all(15),
+            child: AssetThumb(
+              asset: asset,
+              width: 300,
+              height: 300,
+            ),
           );
         }),
       );
@@ -72,7 +78,7 @@ class _ProductImageUploadState extends State<ProductImageUpload> {
         children: <Widget>[
           Center(
             child: Text(
-              "ภาพผลิตภัณฑ์ทุเรียนสำหรับประชาสัมพันธ์",
+              "ภาพผลิตภัณฑ์สำหรับประชาสัมพันธ์",
               style: TextStyle(fontSize: 20),
             ),
           ),
@@ -89,7 +95,7 @@ class _ProductImageUploadState extends State<ProductImageUpload> {
             ),
           ),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               FloatingActionButton(
                 onPressed: () => loadAssets(),
@@ -102,34 +108,4 @@ class _ProductImageUploadState extends State<ProductImageUpload> {
       ),
     );
   }
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     appBar: AppBar(
-  //       title: Text("Upload product image"),
-  //     ),
-  //     body: Column(
-  //       children: <Widget>[
-  //         Center(child: Text('Error: $_error')),
-  //         Expanded(
-  //           child: buildGridView(),
-  //         ),
-  //         RaisedButton(
-  //           child: Text("Pick images"),
-  //           onPressed: loadAssets,
-  //         ),
-  //         Row(
-  //           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-  //           children: <Widget>[
-  //             FloatingActionButton(
-  //               onPressed: () => loadAssets(),
-  //               tooltip: "Pick Image",
-  //               child: Icon(Icons.camera_alt),
-  //             ),
-  //           ],
-  //         ),
-  //       ],
-  //     ),
-  //   );
-  // }
 }
