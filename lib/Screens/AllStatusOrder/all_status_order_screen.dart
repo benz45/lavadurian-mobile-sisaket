c6463d73528646977fd9033bf8851f24fe596c21
Phisan Sookkhee <phisan.shukkhi@gmail.com>
2021-04-07 08:53:58 +0700

    create all order screen

--- lib/Screens/AllStatusOrder/all_status_order_screen.dart
+++ lib/Screens/AllStatusOrder/all_status_order_screen.dart
@@ -1,13 +1,26 @@
+import 'package:LavaDurian/Screens/AllStatusOrder/components/body.dart';
 import 'package:flutter/material.dart';
+import 'package:LavaDurian/constants.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class AllStatusOrderScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('All Status Order Screen'),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        title:
+            Text('สถานะคำสั่งซื้อทั้งหมด').text.color(kTextPrimaryColor).make(),
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: Icon(Icons.arrow_back_rounded),
+          color: kPrimaryColor,
+        ),
       ),
-      body: Text('Coming soon...'),
+      body: SingleChildScrollView(
+        child: Body(),
+      ),
     );
   }
 }
